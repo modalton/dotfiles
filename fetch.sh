e51a29399058db472545e5fc9f92a196ff5adcd0
Michael Dalton <modintexas@gmail.com>
2018-05-11 17:45:29 -0700

    Fri May 11 17:45:29 PDT 2018

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,4 @@
-#Grab cop dotfiles
+#Grab all dotfiles
 
 for i in ".emacs" ".bashrc" ".tern-config"
 do
@@ -6,6 +6,18 @@ do
     cp ~/${i} ./
 done
 
-# pipe all dot files into git add. Luckily git isnt dumb enough to add itself.
-# add regex exemption for git at some point just to be robust
-ls -d .!(|.) || git add
+# Luckily git isnt dumb enough to add itself. add regex exemption for git at some point just to be robust!
+git add .\!\(|.\)
+git add fetch.sh
+
+
+
+
+
+if [ $1 ];then
+    git commit -m "$1"
+else
+    git commit -m "`date`"
+fi
+
+git push origin master
